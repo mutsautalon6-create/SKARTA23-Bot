77188632ebe1278d38d4fc4014305a879f716f00
TALON <mutsautalon6@gmail.com>
2025-11-04 07:50:12 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+const settings = {
+  packname: 'SKARTA23-Bot',
+  author:'Talon Mutsau',
+  botName: "SKARTA23-Bot",
+  botOwner: 'Mutsau Talon', // Talon Mutsau
+  ownerNumber: '263787780586', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+ updateZipUrl: "https://github.com/mutsautalon6-create/SKARTA23/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
